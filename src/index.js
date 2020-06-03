d12fe5a335aa040ca5d6cd7bdd46196a8d5a3a24
Stephanie Cheney <steph@scheneyphotography.com>
2020-06-03 16:11:56 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+function createList(comment) {
+const list = document.createElement('li')
+list.textContent=comment; 
+
+const todo= document.querySelector("ul");
+todo.append(list);
+}
+
+const form=document.querySelector("form")
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const comment= document.querySelector('#new-task-description').value;
+
+  createList(comment);
+  form.reset(); 
+});

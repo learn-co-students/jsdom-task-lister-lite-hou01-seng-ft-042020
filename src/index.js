d9a32edd79e62e21784a09b902e542a4d8d4b120
estherkimyunjung <estherkimyunjung@gmail.com>
2020-06-03 17:46:37 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  function createTodoList(desc){
+    const li = document.createElement('li')
+    const ul = document.getElementById("tasks");
+    li.textContent = desc;
+    ul.append(li);
+    const btn = document.createElement('button');
+    btn.textContent = 'X'
+    li.append(btn);
+    btn.addEventListener('click', function(e){
+      e.preventDefault();
+      li.remove();
+    });
+  }
+
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const list = document.getElementById("new-task-description").value;
+    createTodoList(list);
+    form.reset();
+  });
+
+
 });
+
+  // fetch(`${url}`)
+  // .then(res => res.json())
+  // .then(json => {
+  //   for (const list of json) {
+  //     createTodoList(list);
+  //   }
+  // })
